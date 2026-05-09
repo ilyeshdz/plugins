7b13d7e8732dce8b8e6e2047febe835de9928267
Ilyes Hernandez <hdz.ilyes@gmail.com>
2026-05-09 22:13:48 +0200

    feat(ssg): configure base URL and custom minimal styling

--- lune.config.js
+++ lune.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from "@uwu/lune";
 
 export default defineConfig({
   builder: "rolldown",
+  ssg: {
+    repo_name: "Shelter Plugins",
+    base_url: "https://ilyeshdz.github.io/plugins",
+  },
 });
